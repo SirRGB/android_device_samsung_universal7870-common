f744607ae72112e13418ecd6472e949f8c59da44
SirRGB <sirrgb@proton.me>
2022-09-29 18:29:11 +0200

    [TEMP] universal7870-common: set selinux permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,7 @@ TARGET_LINUX_KERNEL_VERSION := 3.18
 
 # Kernel config
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7870
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # HIDL
 DEVICE_MATRIX_FILE := $(LOCAL_PATH)/configs/compatibility_matrix.xml
